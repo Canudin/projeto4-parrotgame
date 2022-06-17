ad9f9e819585406936677fd64e354101815d89f1
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-06-17 20:56:24 -0300

    Animação cartas v2

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const birdArray = [
   "./img/quack.svg",
 ];
 const idArray = [0, 1, 2, 3, 4, 5, 6];
-const cardArray = [];
+let cardArray = [];
 let cards = 0;
+const table = document.querySelector(".table");
+let clicks = document.querySelectorAll(".clicked");
 
 function random() {
   return Math.random() - 0.5;
@@ -18,10 +20,9 @@ function random() {
 function numberCards() {
   let n = 0;
   let m = 0;
-  let o = 0;
+  cardArray = [];
 
   //Card Number Request
-  const table = document.querySelector(".table");
   table.innerHTML = "";
   cards = prompt("Qual o número de cartas que você quer jogar?");
 
@@ -40,38 +41,37 @@ function numberCards() {
   cardArray.sort(random);
   console.log(cardArray);
 
-  while (o < cards) {
+  while (m < cards) {
     table.innerHTML += `
-        <div class="card"
-        <div class="front-face face" onclick="turn(this)">
-        <img src="./img/parrot.png" />
-        </div>
-        <div class="back-face face">
-        <img src="${birdArray[cardArray[o]]}" />
-        </div>
-        </div>`;
-    o++;
-  }
-
-  /*
-  //Front Generation
-  while (o < cards) {
-    table.innerHTML += `
-        <div class="card" onclick="turn(this)">
-        <img src="./img/parrot.png" />
+        <div class="card" onclick="turned(this)">
+            <div class="front-face face pair${cardArray[m]}">
+                <img src="./img/parrot.png" />
+            </div>
+            <div class="back-face face">
+                <img src="${birdArray[cardArray[m]]}" />
+            </div>
         </div>`;
     m++;
   }
-
-  //Back Generation
-  while (o < cards) {
-    table.innerHTML += `
-    <div class="card" class="turned_card">
-    <img src="${birdArray[cardArray[o]]}" />
-    </div>`;
-    o++;
-  }
-  */
 }
 
-function turn(element) {}
+function turned(element) {
+  const back = element.querySelector(".back-face");
+  const front = element.querySelector(".front-face");
+  element.classList.add("clicked");
+  front.classList.add("turnedfront");
+  back.classList.add("turnedback");
+  while (clicks.length < 2) {
+    
+  }
+  setTimeout(unturn, 1000);
+}
+
+function unturn(){
+  const element = document.querySelector(".clicked");
+  const back = element.querySelector(".back-face");
+  const front = element.querySelector(".front-face");
+  element.classList.remove("clicked");
+  front.classList.remove("turnedfront");
+  back.classList.remove("turnedback");
+}
